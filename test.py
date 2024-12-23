19c925d1c3a19089cdbe21c352aa1f5cd8a017bb
garv sachdev <83745829+gavkujo@users.noreply.github.com>
2024-12-23 19:00:53 +0800

    Update test.py
    added IoU metric

--- test.py
+++ test.py
@@ -15,10 +15,10 @@ try:
 except:
     pass
 from utils import test_single_volume
-#from networks.vit_seg_modeling import VisionTransformer as ViT_seg
-#from networks.vit_seg_modeling import CONFIGS as CONFIGS_ViT_seg
-from networks import VisionTransformer as ViT_seg
-from networks import CONFIGS as CONFIGS_ViT_seg
+from networks.vit_seg_modeling import VisionTransformer as ViT_seg
+from networks.vit_seg_modeling import CONFIGS as CONFIGS_ViT_seg
+#from networks import VisionTransformer as ViT_seg
+#from networks import CONFIGS as CONFIGS_ViT_seg
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--volume_path', type=str,
@@ -47,7 +47,7 @@ parser.add_argument('--seed', type=int, default=1234, help='random seed')
 parser.add_argument('--vit_patches_size', type=int, default=16, help='vit_patches_size, default is 16')
 args = parser.parse_args()
 
-
+'''
 def inference(args, model, test_save_path=None):
     db_test = args.Dataset(base_dir=args.volume_path, split="test_vol", list_dir=args.list_dir)
     testloader = DataLoader(db_test, batch_size=1, shuffle=False, num_workers=1)
@@ -68,6 +68,37 @@ def inference(args, model, test_save_path=None):
     mean_hd95 = np.mean(metric_list, axis=0)[1]
     logging.info('Testing performance in best val model: mean_dice : %f mean_hd95 : %f' % (performance, mean_hd95))
     return "Testing Finished!"
+'''
+def inference(args, model, test_save_path=None):
+    db_test = args.Dataset(base_dir=args.volume_path, split="test_vol", list_dir=args.list_dir)
+    testloader = DataLoader(db_test, batch_size=1, shuffle=False, num_workers=1)
+    logging.info("{} test iterations per epoch".format(len(testloader)))
+    model.eval()
+    metric_list = 0.0
+    for i_batch, sampled_batch in tqdm(enumerate(testloader)):
+        h, w = sampled_batch["image"].size()[2:]
+        image, label, case_name = sampled_batch["image"], sampled_batch["label"], sampled_batch['case_name'][0]
+        dice_scores, hd95_scores, iou_scores = test_single_volume(
+            image, label, model, classes=args.num_classes, 
+            patch_size=[args.img_size, args.img_size],
+            test_save_path=test_save_path, case=case_name, z_spacing=args.z_spacing
+        )
+        metric_list += np.array([dice_scores, hd95_scores, iou_scores])
+        logging.info('idx %d case %s mean_dice %f mean_hd95 %f mean_iou %f' % (
+            i_batch, case_name, np.mean(dice_scores), np.mean(hd95_scores), np.mean(iou_scores)
+        ))
+    metric_list = metric_list / len(db_test)
+    for i in range(1, args.num_classes):
+        logging.info('Mean class %d mean_dice %f mean_hd95 %f mean_iou %f' % (
+            i, metric_list[i-1][0], metric_list[i-1][1], metric_list[i-1][2]
+        ))
+    performance = np.mean(metric_list, axis=0)[0]
+    mean_hd95 = np.mean(metric_list, axis=0)[1]
+    mean_iou = np.mean(metric_list, axis=0)[2]
+    logging.info('Testing performance in best val model: mean_dice : %f mean_hd95 : %f mean_iou : %f' % (
+        performance, mean_hd95, mean_iou
+    ))
+    return "Testing Finished!"
 
 
 
@@ -86,8 +117,8 @@ if __name__ == "__main__":
 
     dataset_config = {
         'ACDC': {
-            'Dataset': ACDC_dataset,  # datasets.dataset_acdc.BaseDataSets,
-            'volume_path': '../data/ACDC',
+            #'Dataset': ACDC_dataset,  # datasets.dataset_acdc.BaseDataSets,
+            'volume_path': './data/ACDC',
             'list_dir': None,
             'num_classes': 4,
             'z_spacing': 5,
